9d888cec65100654793d771385de6d31beea5f20
doyeoo <ehdusla013@gmail.com>
2022-02-22 02:15:28 +0900

    CORS 설정

--- linkproject/linkproject/settings.py
+++ linkproject/linkproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp.apps.MainappConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST=('http://127.0.0.1:3000','http://localhost:3000')
+CORS_ALLOW_CREDENTIALS=True
 
 ROOT_URLCONF = 'linkproject.urls'
 
